7c20f88fd498f6f5c7f6124352350d0bf41e06bc
LeoWelzel <leowelzel2002@gmail.com>
2020-07-31 16:26:16 +0100

    removed testing functions from main

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 
 #include "tests.h"
-#include "temptest.h"
-
-void test()
-{
-    TempTest tempTest;
-
-    for (int i = 0; i < 15; i++)
-        tempTest.doubleSize();
-    std::cout << "Done.\n";
-    std::cout.flush();
-}
 
 int main()
 {
