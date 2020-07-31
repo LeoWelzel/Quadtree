f00371572c23bb155b1501c6de1e987c0f178e6a
LeoWelzel <leowelzel2002@gmail.com>
2020-07-31 12:39:10 +0100

    added defs file

---
+++ source/defs.h
@@ -0,0 +1,10 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+#define ASSERTIONS
+
+#ifdef ASSERTIONS
+    #include <cassert>
+#endif
+
+#endif
--- source/freelist.h
+++ source/freelist.h
@@ -3,6 +3,8 @@
 
 #include <cstddef>
 
+#include "defs.h"
+
 /* Simple memory allocator. Also supports usage as a stack, though this must not be mixed with
  * freelist usage. */
 template<typename TypeName, const size_t FixedSize = 128>
