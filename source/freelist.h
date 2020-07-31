84984545268dae03b48e38fdbfc6465e2c8f2c9e
LeoWelzel <leowelzel2002@gmail.com>
2020-07-31 12:28:34 +0100

    added freelist attributes

--- source/freelist.h
+++ source/freelist.h
@@ -10,9 +10,23 @@ class FreeList
 {
 public:
     FreeList();
+    ~FreeList();
 
 private:
+    /* Allocate memory on the stack to avoid heap allocation until necessary. */
     TypeName fixed[FixedSize];
+
+    /* Points to the data being used in the freelist. */
+    TypeName* dataPtr;
+
+    /* Indicates the freelist capacity. */
+    int capacity;
+
+    /* The number of elements stored. */
+    int numElements;
+
+    /* The index of the earliest free element in the freelist. */
+    int freeElement;
 };
 
 #endif
