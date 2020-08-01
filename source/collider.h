fb16cdb87947aab14960f0ac8b327925cfd29976
LeoWelzel <leowelzel2002@gmail.com>
2020-08-01 09:55:37 +0100

    added collider and quadtree headers

---
+++ source/collider.h
@@ -0,0 +1,22 @@
+#ifndef COLLIDER_H_INCLUDED
+#define COLLIDER_H_INCLUDED
+
+#include "defs.h"
+
+/* Rectangle used to indicate the boundaries of a collider. Such a structure can also be used */
+/* to house any other type of polygon. */
+struct QuadtreeCollider
+{
+    /* Default constructor needed for initialisation of freelists. */
+    QuadtreeCollider();
+
+    QuadtreeCollider(int top, int bottom, int left, int right);
+
+    int top, bottom, left, right;
+
+    #ifdef TO_STRING
+        std::string toString() const;
+    #endif
+};
+
+#endif
---
+++ source/quadtree.h
@@ -0,0 +1,17 @@
+#ifndef QUADTREE_H_INCLUDED
+#define QUADTREE_H_INCLUDED
+
+#include "defs.h"
+#include "freelist.h"
+#include "freestack.h"
+
+class Quadtree
+{
+public:
+    Quadtree();
+
+private:
+
+};
+
+#endif
